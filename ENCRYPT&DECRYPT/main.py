810ad77753c3f1589f479e344703da37a1dfab59
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-11-26 09:50:27 +0530

    ew

--- ENCRYPT&DECRYPT/main.py
+++ ENCRYPT&DECRYPT/main.py
@@ -1,6 +1,13 @@
 from cryptography.fernet import Fernet
 
-key=Fernet.generate_key()
+# key=Fernet.generate_key()
 
-with open ('kuchbhi.key', 'wb') as kuchbhi:
-    kuchbhi.write(key)
+# with open ('kuchbhi.key', 'wb') as kuchbhi:
+#     kuchbhi.write(key)
+
+
+    # kuchbhi keyfile this file is contaning actual encryption key encrypt& decrypt csv file
+
+with open('kuchbhi.key','rb') as kuchbhi:
+    key= kuchbhi.read()
+print(key)
