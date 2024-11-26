9662d1f174138ac688e8637a6a32b7aaa7002f95
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-11-26 10:02:07 +0530

    n

--- ENCRYPT&DECRYPT/main.py
+++ ENCRYPT&DECRYPT/main.py
@@ -11,3 +11,13 @@ from cryptography.fernet import Fernet
 with open('kuchbhi.key','rb') as kuchbhi:
     key= kuchbhi.read()
 print(key)
+
+f=Fernet(key)
+
+with open('student.csv','rb') as original_file:
+    original = original_file.read ()
+
+encrypted=f.encrypt(original)
+
+with open('enc_student.csv', 'wb') as encrypted_file:
+    encrypted_file.write(encrypted)
